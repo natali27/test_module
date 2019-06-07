c021e0882dc9decfc47de58311a22745a185f635
n.virtseva <n.virtseva@norsi-trans.ru>
2019-06-07 18:44:31 +0300

    initial commit

---
+++ src/map.js
@@ -0,0 +1,12 @@
+export class LeafletMap {
+
+// export function LeafletMap(mapOptions) {
+  constructor(mapOptions) {
+	console.log('test module gone');
+  }
+
+  testFunc() {
+	return 5;
+  }
+};
+
