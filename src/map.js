16893782dfaaab2a4b054e3f9a7149d00763ce4e
n.virtseva <n.virtseva@norsi-trans.ru>
2019-06-10 12:15:06 +0300

    try update

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@ export class LeafletMap2 {
 
 // export function LeafletMap(mapOptions) {
   constructor(mapOptions) {
-	console.log('test module gone');
+	console.log('test_module is ok');
   }
 
   testFunc() {
